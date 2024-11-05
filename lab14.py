eabf673df14f115dfdd740ddf0a4f1098a61179d
Joe <josephbarrett19991@gmail.com>
2024-11-05 17:42:37 +0000

    join and split

---
+++ lab14.py
@@ -0,0 +1,15 @@
+#!/user/bin/pyhton3
+
+def ask():
+
+    user_name = input("what is your name? :")
+
+    day_week = input("What day is it today? :")
+
+    print("Hello,", user_name + "!", "Happy", day_week + "!")
+
+  # print(f'Hello, {user_name}! Happy {day_week}!')
+
+ask()
+
+    
