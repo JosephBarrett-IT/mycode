8f96a9bb432795fc21f6a99c40019d6347ff5439
Joe <josephbarrett19991@gmail.com>
2024-11-05 19:26:18 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+my_list = ["192.168.0.5", 5060, "UP"]
+
+#Print first item from list
+print(f"The first item in the list (IP): {my_list[0]}")
+
+#Print second item from list
+print(f"The secound item in the list (port): {str(my_list[1])}" )
+
+#print thrid item from list
+print(f"The last item in the list (state): {my_list[2]} ")
+
+
---
+++ mix-list/mixlist02.py
@@ -0,0 +1,5 @@
+#!/user/bin/env python3
+
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print(f"Vaild IP's from list \n {iplist[3]} \n {iplist[4]}")
